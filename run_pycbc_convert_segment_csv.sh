49c3b5a77c68be32a31925bb7dc386ab4b65d556
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-01-09 11:39:14 -0800

    Add ODC channel to output file.

--- run_pycbc_convert_segment_csv.sh
+++ run_pycbc_convert_segment_csv.sh
@@ -2,4 +2,6 @@
 
 cat test/check_exc/*.txt > test/check_exc/H1L1-All.txt
 
+cat test/check_odc/*.txt > test/check_odc/H1L1-All.txt
+
 python pycbc_convert_segment_csv --segment-files test/check_segdb/*.xml.gz --output-file test/check_segdb/H1L1-ALL.txt
