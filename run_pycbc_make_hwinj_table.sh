b3abc166cb8bb50e3ccf28ece1f0e7f5a08e6ee2
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-01-09 11:54:35 -0800

    Added ODC bit to table.

--- run_pycbc_make_hwinj_table.sh
+++ run_pycbc_make_hwinj_table.sh
@@ -7,12 +7,14 @@ OUTPUT_FILE=/home/cbiwer/public_html/tmp.html
 
 SEGMENT_FILE=test/check_segdb/H1L1-ALL.txt
 EXCITATION_FILE=test/check_exc/H1L1-All.txt
+ODC_FILE=test/check_odc/H1L1-All.txt
 GRACEDB_FILE=test/check_gracedb/H1L1-GRACEDB-1128672300-400.txt
 SCHEDULE_FILE=test/check_schedule/H1L1-SCHEDULE-1128672300-400.txt
 
 python pycbc_make_hwinj_table --start-time ${START_TIME} --end-time ${END_TIME} \
     --segment-file ${SEGMENT_FILE} \
     --excitation-file ${EXCITATION_FILE} \
+    --odc-file ${ODC_FILE} \
     --gracedb-file ${GRACEDB_FILE} \
     --schedule-file ${SCHEDULE_FILE} \
     --output-file ${OUTPUT_FILE}
