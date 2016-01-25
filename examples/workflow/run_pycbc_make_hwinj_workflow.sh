7e2335207b2fc41372391b829391abc9da062380
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-01-25 13:40:36 -0800

    Update config file to run on more bitmasks. Limit number of submitted jobs in workflow to 16.

--- examples/workflow/run_pycbc_make_hwinj_workflow.sh
+++ examples/workflow/run_pycbc_make_hwinj_workflow.sh
@@ -5,12 +5,14 @@ CONFIG_FILE=${PWD}/config.ini
 
 IFO1=H1
 IFO2=L1
+#START_TIME=1136419217
+#END_TIME=$((START_TIME + 604800))
 START_TIME=1128672300
 END_TIME=1128672700
 
 TINJ_SCHEDULE_PATH=${PWD}/tinj/schedule
 
-svn co https://daqsvn.ligo-la.caltech.edu/svn/injection/hwinj/Details/tinj/
+#svn co https://daqsvn.ligo-la.caltech.edu/svn/injection/hwinj/Details/tinj/
 
 # change into run dir
 mkdir -p ${WORKFLOW_NAME}
@@ -24,6 +26,9 @@ pycbc_make_hwinj_workflow --name ${WORKFLOW_NAME} \
         workflow-ifos:${IFO2} \
         workflow-schedule:schedule-path:${TINJ_SCHEDULE_PATH}
 
+# limit workflow to only 16 jobs
+export _CONDOR_DAGMAN_MAX_JOBS_SUBMITTED=16
+
 # plan and submit the workflow
 cd ${WORKFLOW_NAME}
-pycbc_submit_dax --accounting-group ligo.dev.o1.detchar.explore.test --dax ${WORKFLOW_NAME}.dax --no-create-proxy
+pycbc_submit_dax --accounting-group ligo.dev.o1.detchar.explore.test --dax ${WORKFLOW_NAME}.dax
