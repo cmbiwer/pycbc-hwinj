8f230727ff6fc3b6e27502a16274a48e96b50c7a
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-02-16 13:48:32 -0800

    New tab for links on page.

--- examples/workflow/run_pycbc_make_hwinj_workflow.sh
+++ examples/workflow/run_pycbc_make_hwinj_workflow.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-WORKFLOW_NAME=test
+WORKFLOW_NAME=test2
 CONFIG_FILE=${PWD}/config_h1.ini
 
 IFO1=H1
 #IFO2=L1
-START_TIME=1136419217
-END_TIME=$((START_TIME + 604800))
-#START_TIME=1128672300
-#END_TIME=1128672700
+#START_TIME=1136419217
+#END_TIME=$((START_TIME + 604800))
+START_TIME=1128672300
+END_TIME=1128672700
 
 TINJ_SCHEDULE_PATH=${PWD}/tinj/schedule
 
@@ -31,4 +31,4 @@ export _CONDOR_DAGMAN_MAX_JOBS_SUBMITTED=16
 
 # plan and submit the workflow
 cd ${WORKFLOW_NAME}
-pycbc_submit_dax --accounting-group ligo.dev.o1.detchar.explore.test --dax ${WORKFLOW_NAME}.dax
+#pycbc_submit_dax --accounting-group ligo.dev.o1.detchar.explore.test --dax ${WORKFLOW_NAME}.dax
