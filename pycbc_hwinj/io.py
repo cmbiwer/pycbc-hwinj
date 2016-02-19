b0261cb1ebf5b7b38c121ffce359b1233cf6750d
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-02-19 12:03:11 -0800

    Make a single executable and class for I/O frame data.

--- pycbc_hwinj/io.py
+++ pycbc_hwinj/io.py
@@ -1,20 +1,26 @@
 from glue import segments
 
-class ExcitationEntry(object):
+class DataEntry(object):
 
-    def __init__(self, channel_name, seg):
+    def __init__(self, channel_name, seg, description="", bitmask=""):
         self.channel_name = channel_name
         self.seg = seg
+        self.description = description
+        self.bitmask = bitmask
 
     def write(self):
-        vals = [self.channel_name, self.seg[0], self.seg[1]]
+        vals = [self.channel_name, self.seg[0], self.seg[1],
+                self.description, self.bitmask]
         return ",".join(map(str, vals))
 
-def read_exc_entry(line, delimiter=","):
+def read_entry(line, delimiter=","):
     data = line.split(delimiter)
     i = 0
     channel_name = data[i]; i += 1
     seg = segments.segment(float(data[i]), float(data[i+1])); i += 2
-    return ExcitationEntry(channel_name, seg)
+    description = data[i]; i += 1
+    bitmask = data[i]; i += 1
+    return DataEntry(channel_name, seg, description=description,
+                           bitmask=bitmask)
 
 
