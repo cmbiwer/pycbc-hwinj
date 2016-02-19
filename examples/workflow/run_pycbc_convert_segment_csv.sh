5d4322847698d3b9a64b6b744c3f647c703c2af3
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-02-19 13:57:11 -0800

    Remove old run script.

--- examples/workflow/run_pycbc_convert_segment_csv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-RUN_DIR=test2/test2
-
-cat ${RUN_DIR}/check_exc/*.txt > ${RUN_DIR}/check_exc/H1L1-All.txt
-
-cat ${RUN_DIR}/check_bitmask/*.txt > ${RUN_DIR}/check_bitmask/H1L1-All.txt
-
-pycbc_convert_segment_csv --segment-files ${RUN_DIR}/check_segdb/*.xml.gz --output-file ${RUN_DIR}/check_segdb/H1L1-ALL.txt
